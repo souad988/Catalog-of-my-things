fe2e412482b6f8167f0980c0404e4fae6070bb54
souad988 <59707859+souad988@users.noreply.github.com>
2022-05-23 16:44:30 +0100

    Create can_be_archived and move_to_archive methodes

--- item.rb
+++ item.rb
@@ -1,9 +1,10 @@
+require 'date'
 class Item
   attr_accessor :publish_date
 
   def initialize(publish_date, archived, id = nil)
     @id = id.nil? ? Random.rand(1..1000) : id
-    @publish_date = publish_date
+    @publish_date = Date.parse(publish_date)
     @archived = archived
   end
 
@@ -21,6 +22,14 @@ class Item
 
   def add_genre(genre)
     @genre = genre
+  end
+
+  def can_be_archived?
+    Date.today.prev_year(10) > @publish_date
+  end
+
+  def move_to_archive()
+    @archived = true if can_be_archived?
   end
 
   private
