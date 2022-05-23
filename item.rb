944a2805f2ab0b6045cfa689ec6496ddfc0eb5af
souad988 <59707859+souad988@users.noreply.github.com>
2022-05-23 14:04:00 +0100

    Add getters and setters for 1 to many relation attributes

--- item.rb
+++ item.rb
@@ -1,8 +1,30 @@
 class Item
-    def initialize(publish_date, archived, id=nil)
-        @id = id.nil? ? Random.rand(1..1000) : id
-        @publish_date = publish_date
-        @archived = archived
-    end
-    
+  attr_accessor :publish_date
+
+  def initialize(publish_date, archived, id = nil)
+    @id = id.nil? ? Random.rand(1..1000) : id
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def add_source(source)
+    @source = source
+  end
+
+  def add_label(label)
+    @label = label
+  end
+
+  def add_author(author)
+    @author = author
+  end
+
+  def add_genre(genre)
+    @genre = genre
+  end
+
+  private
+
+  attr_accessor :archived
+  attr_reader :id
 end
