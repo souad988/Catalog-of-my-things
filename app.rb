06a0fe49347e4f1a5939cc3444f028dbd2f8cd38
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-05-24 14:10:25 +0000

    Update options and save to file

--- app.rb
+++ app.rb
@@ -1,7 +1,42 @@
 require_relative './inputs'
+require_relative './item'
+require_relative './games_module'
+require_relative './authors_module'
+require_relative './classes/game'
+require_relative './classes/author'
+
 
 class App
   include Inputs
+  include GamesModule
+  include AuthorsModule
+
+  def initialize
+    @games = load_games
+    @authors = load_authors
+  end
+
+  def list_all_authors
+    @authors.each do |author|
+      puts "#{author.first_name} #{author.last_name}"
+    end
+  end
+  
+  def list_all_games
+    @games.each do |game|
+      puts "#{game.name} #{game.publish_date}"
+    end
+  end
+
+  def add_game 
+    multiplayer = input_bool()
+    last_played_at = input_string()
+    publish_date = input_string()
+    game = Game.new(multiplayer, last_played_at, publish_date)
+    @games.push(game)
+    puts "Game added successfully!"
+    end
+end
 
   def display_menu
     options = [
@@ -28,8 +63,8 @@ class App
   def display_choice(option)
     methods = {}
     # Placeholder functions need to be replaced
-    methods[1] = method(:list_all_books)
-    methods[2] = method(:list_all_people)
+    methods[1] = method(:list_all_)
+    methods[2] = method(:list_all_games)
     methods[3] = method(:add_new_person)
     methods[4] = method(:add_new_book)
     methods[5] = method(:add_new_rental)
