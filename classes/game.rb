2ad5f32335e320f5edfed0b6fa8ee7d967e1393c
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-05-24 12:44:23 +0000

    Initialize class

--- classes/game.rb
+++ classes/game.rb
@@ -2,6 +2,10 @@ require 'date'
 require_relative '../item'
 
 class Game < Item 
-    attr_accessor :multiplayer, :last_played, :publish_date
+    attr_accessor :multiplayer, :last_played_date, :publish_date
 
-    
+    def initialize(multiplayer, last_played_date, publish_date)
+        super(id, name, publish_date)
+        @multiplayer = multiplayer
+        @last_played_date = Date.parse(last_played_date)
+    end
