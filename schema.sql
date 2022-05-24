5c1103ad74d021c040b5f4439fc1f835ca7edc4b
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-05-24 12:49:24 +0000

    Create table for the items

--- schema.sql
+++ schema.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE catalog;
+
+CREATE TABLE item (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    publish_date DATE,
+    archived BOOLEAN,
+    PRIMARY KEY (id),
+    FOREIGN KEY (label_id) REFERENCES label(id)
+    FOREIGN KEY (genre_id) REFERENCES genre(id),
+    FOREIGN KEY (author_id) REFERENCES author(id),
+)
