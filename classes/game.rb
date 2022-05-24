8d8dcf85448ee9c56db7bb1734638cf5c6ccedfa
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-05-24 12:47:18 +0000

    Create method for adding to archives

--- classes/game.rb
+++ classes/game.rb
@@ -2,10 +2,15 @@ require 'date'
 require_relative '../item'
 
 class Game < Item 
-    attr_accessor :multiplayer, :last_played_date, :publish_date
+    attr_accessor :multiplayer, :last_played_at, :publish_date
 
-    def initialize(multiplayer, last_played_date, publish_date)
+    def initialize(multiplayer, last_played_at, publish_date)
         super(id, name, publish_date)
         @multiplayer = multiplayer
-        @last_played_date = Date.parse(last_played_date)
+        @last_played_at = Date.parse(last_played_at)
+    end
+
+    def can_be_archived?
+        duration = Time.now.year - @last_played_at.year
+        super && duration > 2
     end
