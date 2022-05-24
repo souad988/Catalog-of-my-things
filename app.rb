891d43f9329447d11cc227bb26b26cbc90ab6312
salimabdulai <maouslayer@gmail.com>
2022-05-24 05:16:47 -0400

    Modify options dislay

--- app.rb
+++ app.rb
@@ -35,22 +35,6 @@ class App
       puts 'option 3'
     when 4
       puts 'option 4'
-    when 5
-      puts 'option 5'
-    when 6
-      puts 'option 6'
-    when 7
-      puts 'option 7'
-    when 8
-      puts 'option 8'
-    when 9
-      puts 'option 9'
-    when 10
-      puts 'option 10'
-    when 11
-      puts 'option 11'
-    when 12
-      puts 'option 12'
     end
   end
 end
