8bbd14f0f6bcf4ec24066a65acde56db99023780
souad988 <59707859+souad988@users.noreply.github.com>
2022-05-25 16:25:12 +0100

    Add from_json to_json to MusicAlbum class

--- music_album.rb
+++ music_album.rb
@@ -1,13 +1,21 @@
 require './item'
 class MusicAlbum < Item
-  def initialize(publish_date, on_spotify)
-    super(publish_date)
+  def initialize(publish_date, on_spotify, id = nil)
+    super(publish_date, id)
     @on_spotify = on_spotify
   end
 
   def can_be_archived?
     super.can_be_archived && @on_spotify
   end
+  
+  def self.from_json(json)
+    json = JSON.parse(json) if json.is_a? String
+    MusicAlbum.new(json['publish_date'], json['on_spotify'], json['id'])
+  end
 
+  def to_json(*_args)
+    JSON.generate({ id: @id, publish_date: @publish_date, on_spotify: @on_spotify })
+  end
   
 end
