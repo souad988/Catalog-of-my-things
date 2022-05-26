de50215c2ee3e306c651f9eb3b6cc7d7049eb866
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-05-26 12:56:59 +0000

    Update game.rb file

--- classes/game.rb
+++ classes/game.rb
@@ -5,7 +5,7 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
 
   def initialize(multiplayer, last_played_at, publish_date)
-    super(id, name, publish_date)
+    super(id, publish_date)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
