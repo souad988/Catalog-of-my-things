c20f396ce900c9eec7566d02694d03ebcc228f24
salimabdulai <maouslayer@gmail.com>
2022-05-26 06:02:45 -0400

    Remove comments

--- app.rb
+++ app.rb
@@ -60,20 +60,11 @@ class App
   def add_book
     puts 'Enter Publish date'
     publish_date = input_string
-    # puts 'Enter If Archived ? [Y/N]'
-    # archived = input_bool
     puts 'Enter Publisher'
     publisher = input_string
     puts 'Enter state of cover [bad/good]'
     cover_state = input_string
     book = Book.new(publish_date, publisher, cover_state, nil)
-
-    # # select author / add author
-    # puts "Enter Author's First name"
-    # author_first_name = input_string_no_num
-    # puts "Enter Author's last name"
-    # author_last_name = input_string_no_num
-    # author = Author.new(author_first_name, author_last_name)
 
     # add label / select a label
     puts 'Enter label title'
@@ -82,14 +73,7 @@ class App
     label_color = input_string
     label = Label.new(label_title, label_color)
 
-    # # add genre
-    # puts 'Enter genre name'
-    # genre_name = input_string
-    # genre = Genre.new(genre_name)
-
-    # book.add_author(author)
     book.add_label(label)
-    # book.add_genre(genre)
     @labels << label
     @books << book
     store_data('books.json', @books)
