9edc0c861e0e50fbde2e0c031db5a6e96467e5f3
salimabdulai <maouslayer@gmail.com>
2022-05-26 11:35:11 -0400

    Add more tests for impelmented methods

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../classes/book'
+require 'date'
 
 describe Book do
   context 'When testing the Book class' do
     book = Book.new('2020/02/02', 'publisher', 'bad', nil)
+    book2 = Book.new(Date.today.prev_year(1), 'publisher', 'good', nil)
     it 'check attribute publish date' do
       expect(book.genre).to eq '2020/02/02'
     end
@@ -11,6 +13,14 @@ describe Book do
     end
     it 'check attribute cover state' do
       expect(book.cover).to eq 'bad'
+    end
+
+    it 'check can_be_archived method to return true' do
+      expect(book.can_be_archived?).to be true
+    end
+
+    it 'check can_be_archived method to return true' do
+      expect(book2.can_be_archived?).to be false
     end
   end
 end
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,13 +1,19 @@
 require_relative '../classes/label'
+require_relative '../classes/book'
 
 describe Label do
   context 'When testing the Label class' do
     label = Label.new('label_title', 'blue')
+    book = Book.new('2020/02/02', 'publisher', 'bad', nil)
+    label.add_item(book)
     it 'label title check' do
       expect(label.title).to eq 'label_title'
     end
     it 'label color check' do
       expect(label.color).to eq 'blue'
+    end
+    it 'check add_item method in the Label class' do
+      expect(label.items.length).to eq 1
     end
   end
 end
