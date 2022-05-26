2db62b665cb1920d446618ca4b9854cad48ca60e
salimabdulai <maouslayer@gmail.com>
2022-05-26 12:20:09 -0400

    Add more tests to book and label

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,17 +1,32 @@
+require 'date'
 require_relative '../classes/book'
 require_relative '../classes/item'
 
 describe Book do
   context 'When testing the Book class' do
-    book = Book.new('2020/02/02', 'publisher', 'bad', nil)
+    publish_date = '02/02/2020'
+    cover_state = 'bad'
+
+    book = Book.new(publish_date, 'publisher', cover_state, nil)
+    book2 = Book.new(Date.today.prev_year(1), 'publisher', 'bad', nil)
+
     it 'check attribute publish date' do
-      expect(book.genre).to eq '2020/02/02'
+      expect(book.genre).to eql publish_date
     end
     it 'check attribute publisher' do
-      expect(book.publisher).to eq 'publisher'
+      expect(book.publisher).to eql 'publisher'
     end
+
     it 'check attribute cover state' do
-      expect(book.cover).to eq 'bad'
+      expect(book.cover).to eql cover_state
+    end
+
+    it 'can be archived should be true' do
+      expect(book.can_be_archived?).to eql true
+    end
+
+    it 'can be archived should be false' do
+      expect(book2.can_be_archived?).to eql false
     end
   end
 end
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,11 +1,17 @@
 require_relative '../classes/label'
+require_relative '../classes/book'
 
 describe Label do
   context 'When testing the Label class' do
     label = Label.new('label_title', 'label_color')
+    book = Book.new(publish_date, 'publisher', cover_state, nil)
+    label.add_item(book)
     it 'label instance should return attributes it was created with' do
-      expect(label.title).to eq 'label_title'
-      expect(label.color).to eq 'label_color'
+      expect(label.title).to eql 'label_title'
+      expect(label.color).to eql 'label_color'
+    end
+    it 'check length of items to be 1' do
+      expect(label.items.length).to eql 1
     end
   end
 end
